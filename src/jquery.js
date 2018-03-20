3fb0f1e0c322d1ad1ab079db1b03f0c4ea357ca1
Yzing <995498726@qq.com>
2018-03-20 18:13:51 +0800

    study jQuery.Callbacks

--- src/jquery.js
+++ src/jquery.js
@@ -688,6 +688,7 @@ var rootjQuery,
       return cur;
     }
 
+    // jQuery traversal methods
     jQuery.each( {
       parent: function( elem ) {
         var parent = elem.parentNode;
@@ -759,5 +760,149 @@ var rootjQuery,
     		return this.pushStack( matched );
       };
     } )
+
+    var rnothtmlwhite = ( /[^\x20\t\r\n\f]+/g );
+
+    function createOptions( options ) {
+      var object = {};
+      jQuery.each( options.match( rnothtmlwhite ) || [], function( _, flag ) {
+        object[ flag ] = true;
+      } )
+      return object;
+    }
+
+    // Deffer implement, include callback list
+    jQuery.Callbacks = function( options ) {
+      options = typeof options === 'string' ?
+        createOptions( options ) :
+        jQuery.extend( {}, options );
+      var
+        firing,
+        memory,
+        fired,
+        locked,
+        list = [],
+        queue = [],
+        firingIndex = -1,
+        fire = function() {
+          locked = locked || options.once;
+          firing = fired = true;
+          for ( ; queue.length; firingIndex = -1 ) {
+            memory = queue.shift();
+            while ( ++firingIndex < list.length ) {
+              if ( list[ firingIndex ].apply( memory[ 0 ], memory[ 1 ] ) === false && options.stopOnFalse ) {
+                firingIndex = list.length;
+                memory = false;
+              }
+            }
+          }
+
+          if ( !options.memory ) {
+            memory = false;
+          }
+
+          firing = false;
+          if ( locked ) {
+            if ( memory ) {
+              list = [];
+            } else {
+              list = "";
+            }
+          }
+        },
+        self = {
+          add: function() {
+            if ( list ) {
+              if ( memory && !firing ) {
+                firingIndex = list.length - 1;
+                queue.push( memory );
+              }
+              ( function add( args ) {
+                jQuery.each( args, function( _, arg ) {
+                  if ( isFunction( arg ) ) {
+                    if ( !options.unique || !self.has( arg ) ) {
+                      list.push( arg );
+                    }
+                  } else if ( arg && arg.length && toType( arg ) !== 'string' ) {
+                    add( arg );
+                  }
+                } );
+              } )( arguments );
+
+              if ( memeory && !firing ) {
+                fire();
+              }
+            }
+            return this;
+          },
+          remove: function() {
+            jQuery.each( arguments, function( _, arg ) {
+              var index;
+              if ( index = jQuery.inArray( arg, list, index ) > -1 ) {
+                list.splice( index, 1 );
+              }
+              if ( index < firingIndex ) {
+                firingIndex--;
+              }
+            } );
+            return this;
+          },
+          has: function( fn ) {
+            return fn ? jQuery.inArray( fn, list ) > -1 : list.length > 0;
+          },
+          empty: function() {
+            if ( list ) {
+              list = [];
+            }
+            return this;
+          },
+          disable: function() {
+            locked = queue = [];
+            list = memory = "";
+            return this;
+          },
+          disabled: function() {
+            return !list;
+          },
+          lock: function() {
+            locked = queue = [];
+            if ( !memory && !firing ) {
+              list = queue = "";
+            }
+            return this;
+          },
+          locked: function() {
+            return !!locked;
+          },
+          fireWith: function( context, args ) {
+            if ( !locked ) {
+              args = args || [];
+              args = [ context, args.slice ? args.slice(), args ];
+              queue.push( args );
+              if ( !firing ) {
+                fire();
+              }
+            }
+            return this;
+          },
+          fire: function() {
+            self.fireWith( this, arguments );
+            return this;
+          },
+          fired: function() {
+            return !!fired;
+          }
+        };
+
+      return self;
+    }
+
+    function Identity( v ) {
+      return v;
+    }
+
+    function Thrower( ex ) {
+      throw ex;
+    }
 
 module.exports = jQuery
--- test/index.js
+++ test/index.js
@@ -1,4 +1,24 @@
 const $ = require("../dist/jquery-3.3.1");
 
-let ctx = $('div');
-console.log(ctx.index('.b'));
+var list = $.Callbacks();
+
+let n = 0;
+let i = 0;
+
+const fn = function( arg ) {
+  let start = new Date().getTime();
+  let i, j;
+  for ( i = 0; i < 100000; i ++ ) {
+    for ( j = 0; j < 10000; j++ );
+  }
+  n++;
+  console.log( n + ' is updated by: ' +arg );
+  let end = new Date().getTime();
+  console.log( 'execute time: ' + ( end - start ) );
+}
+
+list.add( [ fn, fn ] );
+
+list.fire( 'a' );
+list.fire( 'b' );
+list.fire( 'c' );

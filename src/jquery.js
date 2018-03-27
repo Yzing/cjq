5d86a420dfad558ff30e7630df2127ee7524b64c
Yzing <995498726@qq.com>
2018-03-27 18:08:04 +0800

    1、jQuery Deferred

--- src/jquery.js
+++ src/jquery.js
@@ -912,6 +912,10 @@ var rootjQuery,
     jQuery.extend( {
       Deffered: function( func ) {
 
+        // notify 触发 progress 注册的 callbacks
+        // resolve 触发 done 注册的 callbacks
+        // reject 触发 fail 注册的 callbacks
+        // 同时所有 callbacks 可以通过 then() 注册
         var tuples = [
           [ "notify", "progress", jQuery.Callbacks( "memory" ),
             jQuery.Callbacks( "memory" ), 2 ],
@@ -921,35 +925,252 @@ var rootjQuery,
             jQuery.Callbacks( "once memory" ), 1, "rejected" ]
         ],
 
-          state = "pending",
+        // 初始状态都为 pending，即等待状态
+        state = "pending",
 
-          promise = {
+        // promise 为向外暴露的安全的注册对象
+        promise = {
 
-            state: function() {
-              return state;
-            },
-            always: function() {
-
-            },
-            "catch": function() {
-
-            },
-            pipe: function() {
-
-            },
-            then: function() {
-
-            },
-            promise: function() {
-              
-            }
+          state: function() {
+            return state;
           },
-          deffered = {
+          always: function() {
+            // 同时向 done 和 fail 注册 callbacks
+            deferred.done( arguments ).fail( arguments );
+            return this;
+          },
+          "catch": function( fn ) {
+            // 将 fn 注册为 fail 的 callbacks
+            return promise.then( null, fn );
+          },
+          pipe: function( /* fnDone, fnFail, fnProgress */ ) {
 
-          };
-          jQuery.each( tuples, function() {} );
-      },
-      when: function() {}
-    } );
+            var fns = arguments;
+
+            // return a new deferred's promise, the function will be called before the deferred return;
+            // 返回 promise，其中不含状态转移函数，更安全的处理方式
+            return jQuery.Deffered( function( newDefer ) {
+
+              jQuery.each( tuples, function( i, tuple ) {
+
+                var fn = isFunction( fns[ tuple[ 4 ] ] ) && fns[ tuple[ 4 ] ];
+
+                // deferred.progress(function() { bind to newDefer or newDefer.notify })
+                // 在原来的 deferred 中注册函数
+  							// deferred.done(function() { bind to newDefer or newDefer.resolve })
+  							// deferred.fail(function() { bind to newDefer or newDefer.reject })
+                deferred[ tuple[ 1 ] ]( function() {
+                  // 调用 fn
+                  // arguments 为 deferred 状态转移函数的传参，
+                  // 即调用该注册的 function 时的传参，该参数会引用状态转移函数的参数
+                  var returned = fn && fn.call( this, arguments );
+                  if ( returned && isFunction( returned.promise ) ) {
+                    // 若返回的对象中有 promise 方法，则将 newDefer 的状态转移方法注入，
+                    // 即返回的对象也有异步处理函数，其状态改变会触发 newDefer 的状态改变
+                    // newDefer 的状态会等待 returned 的状态改变
+                    returned.promise()
+                      .progress( newDefer.notify )
+                      .done( newDefer.resolve )
+                      .fail( newDefer.reject )
+                  } else {
+                    // 否则在 this 上下文中去调用状态转移方法
+                    // 如果 fn 存在，就传入 fn 的返回值 returned
+                    // 如果不存在，直接返回 arguments
+                    newDefer[ tuple[ 0 ] + 'With' ](
+                      this,
+                      fn ? [ returned ] : arguments
+                    );
+                  }
+                } );
+              } );
+              // 当方法被调用完成后，变量 fns 置为 null
+              fns = null;
+            } ).promise();
+          },
+          then: function( onFulfilled, onRejected, onProgress ) {
+
+            var maxDepth = 0;
+
+            function resolve( depth, deferred, handler, special ) {
+              return function() {
+                var that = this,
+                  args = arguments,
+                  mightThrow = function() {
+
+                    var returned, then;
+
+                    if ( depth < maxDepth ) {
+                      return;
+                    }
+
+                    returned = handler.apply( that, args );
+
+                    if ( returned === deferred.promise() ) {
+                      throw new TypeError( "Thenable self-resolution" );
+                    }
+
+                    then = returned &&
+  										( typeof returned === "object" ||
+  											typeof returned === "function" ) &&
+  										returned.then;
+
+                    if ( isFunction( then ) ) {
+                      if ( special ) {
+                        then.call(
+                          returned,
+                          resolve( maxDepth, deferred, Indentity, special ),
+                          resolve( maxDepth, deferred, Thrower, special )
+                        );
+                      } else {
+
+                        maxDepth++;
+
+                        then.call(
+                          returned,
+                          resolve( maxDepth, deferred, Indentity, special ),
+                          resolve( maxDepth, deferred, Thrower, special ),
+                          resolve( maxDepth, deferred, Identity,
+  													deferred.notifyWith )
+                        );
+                      }
+                    } else {
+                      if ( handle !== Indentity ) {
+                        that = undefined;
+                        args = [ returned ];
+                      }
+
+                      ( special || deferred.resolveWith )( that, args )
+                    }
+                  },
+                  process = special ?
+                    mightThrow :
+                    function() {
+                      try {
+                        mightThrow()
+                      } catch ( e ) {
+                        if ( jQuery.Deferred.exceptionHook ) {
+                          jQuery.Deferred.exceptionHook( e,
+  													process.stackTrace );
+                        }
+                        if ( depth + 1 >= maxDepth ) {
+
+  												// Only substitute handlers pass on context
+  												// and multiple values (non-spec behavior)
+  												if ( handler !== Thrower ) {
+  													that = undefined;
+  													args = [ e ];
+  												}
+
+  												deferred.rejectWith( that, args );
+  											}
+                      }
+                    };
+
+                if ( depth ) {
+                  process();
+                } else {
+
+  								// Call an optional hook to record the stack, in case of exception
+  								// since it's otherwise lost when execution goes async
+  								if ( jQuery.Deferred.getStackHook ) {
+  									process.stackTrace = jQuery.Deferred.getStackHook();
+  								}
+  								window.setTimeout( process );
+  							}
+              };
+            }
+
+            return jQuery.Deffered( function( newDefer) {
+
+              tuples[ 0 ][ 3 ].add(
+                resolve(
+                  0,
+                  newDefer,
+                  isFunction( onProgress ) ?
+                    onProgress :
+                    Indentity
+                )
+              );
+
+              tuples[ 1 ][ 3 ].add(
+                resolve(
+                  0,
+                  newDefer,
+                  isFunction( onFulfilled ) ?
+                    onFulfilled :
+                    Indentity
+                )
+              );
+
+              tuples[ 2 ][ 3 ].add(
+                resolve(
+                  0,
+                  newDefer,
+                  isFunction( onRejected ) ?
+                    onRejected :
+                    Thrower
+                )
+              );
+
+            } ).promise();
+          },
+          promise: function( obj ) {
+            return obj !== null ? jQuery.extend( obj, promise ) : promise;
+          }
+        },
+        deferred = {};
+        jQuery.each( tuples, function( i, tuple ) {
+          var list = tuple[ 2 ],
+            stateString = tuple[ 5 ];
+          promise[ tuple[ 1 ] ] = list.add;
+
+          if ( stateString ) {
+            list.add(
+    					function() {
+
+    						// state = "resolved" (i.e., fulfilled)
+    						// state = "rejected"
+    						state = stateString;
+    					},
+
+    					// rejected_callbacks.disable
+    					// fulfilled_callbacks.disable
+    					tuples[ 3 - i ][ 2 ].disable,
+
+    					// rejected_handlers.disable
+    					// fulfilled_handlers.disable
+    					tuples[ 3 - i ][ 3 ].disable,
+
+    					// progress_callbacks.lock
+    					tuples[ 0 ][ 2 ].lock,
+
+    					// progress_handlers.lock
+    					tuples[ 0 ][ 3 ].lock
+    				);
+          }
+
+          list.add( tuple[ 3 ].fire );
+
+          deferred[ tuple[ 0 ] ] = function() {
+    				deferred[ tuple[ 0 ] + "With" ]( this === deferred ? undefined : this, arguments );
+    				return this;
+    			};
+
+    			// deferred.notifyWith = list.fireWith
+    			// deferred.resolveWith = list.fireWith
+    			// deferred.rejectWith = list.fireWith
+    			deferred[ tuple[ 0 ] + "With" ] = list.fireWith;
+
+        } );
+
+        promise.promise( deferred );
+
+        if ( func ) {
+          func.call( deferred, deferred );
+        }
+        return deferred;
+    },
+    when: function() {}
+  } );
 
 module.exports = jQuery
--- test/index.js
+++ test/index.js
@@ -1,24 +1,24 @@
 const $ = require("../dist/jquery-3.3.1");
 
-var list = $.Callbacks();
+let defer = $.Deferred();
+let defer2 = $.Deferred();
 
-let n = 0;
-let i = 0;
-
-const fn = function( arg ) {
-  let start = new Date().getTime();
-  let i, j;
-  for ( i = 0; i < 100000; i ++ ) {
-    for ( j = 0; j < 10000; j++ );
+let ndefer = defer.pipe(
+  function( args ) {
+    console.log( args )
+  },
+  function() {
+    console.log( 'fail' )
+  },
+  function( args ) {
+    console.log( args )
+    return args + ' through progress'
   }
-  n++;
-  console.log( n + ' is updated by: ' +arg );
-  let end = new Date().getTime();
-  console.log( 'execute time: ' + ( end - start ) );
-}
+)
 
-list.add( [ fn, fn ] );
+ndefer.progress( function ( args ) {
+  console.log( args )
+} )
 
-list.fire( 'a' );
-list.fire( 'b' );
-list.fire( 'c' );
+defer.notify( 'defer args' )
+defer.notify( 'defer args' )
